d06922dea824d4fa27a79450c6bfa57a0b3e3cfd
Kohei MATSUSHITA <ma2shita+git@ma2shita.jp>
2023-02-25 21:41:24 +0900

    Initial commit

---
+++ test/event_bridge_pipes_simple_logger.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventBridgePipesSimpleLogger from '../lib/event_bridge_pipes_simple_logger-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/event_bridge_pipes_simple_logger-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventBridgePipesSimpleLogger.EventBridgePipesSimpleLoggerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
